14d515c49ab2c5ade9824fff8b4de82bdffb82ab
FaizalTrianto03 <ftrian2226@gmail.com>
2025-10-19 19:41:16 +0700

    Upload Modul3

--- modul2/lib/app/modules/home/controllers/home_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class HomeController extends GetxController {
-  // Counter sederhana (observable) yang dipakai oleh HomeView
-  // untuk menampilkan dan memperbarui nilai secara reaktif.
-  final count = 0.obs;
-
-  void increment() => count.value++;
-}
