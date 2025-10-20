6aa670a956a5acf2758f2f012523c5ef91340a52
Faizal Qadri Trianto <128003049+FaizalTrianto03@users.noreply.github.com>
2025-10-20 14:56:17 +0700

    Update Modul 3 Fix Error

--- modul3/lib/main.dart
+++ modul3/lib/main.dart
@@ -44,7 +44,7 @@ ThemeData _buildLightTheme() {
         letterSpacing: 0.5,
       ),
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       color: Colors.white,
       elevation: 2,
       shadowColor: Colors.black.withOpacity(0.1),
@@ -95,7 +95,7 @@ ThemeData _buildDarkTheme() {
         letterSpacing: 0.5,
       ),
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       color: const Color(0xFF1E1E1E),
       elevation: 2,
       shadowColor: Colors.black.withOpacity(0.3),
